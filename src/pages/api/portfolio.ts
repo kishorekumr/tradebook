7586532f95cc096f1e18fc42fc36e4e59d5b52ac
kishorekumr <kishore.saiprakash@gmail.com>
2024-04-09 20:14:19 +0530

    Update portfolio.ts

--- src/pages/api/portfolio.ts
+++ src/pages/api/portfolio.ts
@@ -5,32 +5,33 @@ const prisma = new PrismaClient();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
-    case `POST`: {
+    case 'POST': {
       try {
         const { body } = req;
         const { symbol, exchange, type } = JSON.parse(body);
-    //     await await prisma.equity.update({
-    //       where: {
-    //         symbol_exchange: {
-    //           symbol,
-    //           exchange,
-    //         },
-    //       },
-    //       data: { recorded: type === `buy` },
-    //     });
-    //     res
-    //       .status(200)
-    //       .send(`Portfolio ${symbol} in ${exchange} updated to database`);
-    //   } catch (err) {
-    //     res.status(500).send(`Error saving to database`);
+        // Uncomment the lines below once you're ready to use them
+        // await prisma.equity.update({
+        //   where: {
+        //     symbol_exchange: {
+        //       symbol,
+        //       exchange,
+        //     },
+        //   },
+        //   data: { recorded: type === 'buy' },
+        // });
+        // res
+        //   .status(200)
+        //   .send(`Portfolio ${symbol} in ${exchange} updated to database`);
+      } catch (err) {
+        res.status(500).send('Error saving to database');
       } finally {
         prisma.$disconnect();
       }
       break;
-    // }
-    // default: {
-    //   res.status(405).end();
-    //   break;
-    // }
+    }
+    default: {
+      res.status(405).end();
+      break;
+    }
   }
 };
