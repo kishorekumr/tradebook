28176e5260d570b49804cae557b67c167cfa2e08
kishorekumr <kishore.saiprakash@gmail.com>
2024-04-09 20:21:43 +0530

    Update portfolio.ts

--- src/pages/api/portfolio.ts
+++ src/pages/api/portfolio.ts
@@ -3,35 +3,35 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.method) {
-    case 'POST': {
-      try {
-        const { body } = req;
-        const { symbol, exchange, type } = JSON.parse(body);
-        // Uncomment the lines below once you're ready to use them
-        // await prisma.equity.update({
-        //   where: {
-        //     symbol_exchange: {
-        //       symbol,
-        //       exchange,
-        //     },
-        //   },
-        //   data: { recorded: type === 'buy' },
-        // });
-        // res
-        //   .status(200)
-        //   .send(`Portfolio ${symbol} in ${exchange} updated to database`);
-      } catch (err) {
-        res.status(500).send('Error saving to database');
-      } finally {
-        prisma.$disconnect();
-      }
-      break;
-    }
-    default: {
-      res.status(405).end();
-      break;
-    }
-  }
-};
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   switch (req.method) {
+//     case 'POST': {
+//       try {
+//         const { body } = req;
+//         const { symbol, exchange, type } = JSON.parse(body);
+//         // Uncomment the lines below once you're ready to use them
+//         // await prisma.equity.update({
+//         //   where: {
+//         //     symbol_exchange: {
+//         //       symbol,
+//         //       exchange,
+//         //     },
+//         //   },
+//         //   data: { recorded: type === 'buy' },
+//         // });
+//         // res
+//         //   .status(200)
+//         //   .send(`Portfolio ${symbol} in ${exchange} updated to database`);
+//       } catch (err) {
+//         res.status(500).send('Error saving to database');
+//       } finally {
+//         prisma.$disconnect();
+//       }
+//       break;
+//     }
+//     default: {
+//       res.status(405).end();
+//       break;
+//     }
+//   }
+// };
